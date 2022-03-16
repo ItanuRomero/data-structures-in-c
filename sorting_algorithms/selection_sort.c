32239ff6182e93cd3c42956eba7eedc4af82823b
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2022-03-16 20:29:09 -0300

    change var names

--- sorting_algorithms/selection_sort.c
+++ sorting_algorithms/selection_sort.c
@@ -11,19 +11,19 @@ void print_vector(int array[], int size) {
 }
 
 void selection_sort(int vet[], int size) {
-	int position, number, aux, smaller, smaller_position;
-	for(position = 0; position < size - 1; position++) {
-		smaller = vet[position];
-		smaller_position = position;
-		for(number = position; number < size; number++) {
-			if (vet[number] < smaller) {
-				smaller = vet[number];
-				smaller_position = number;
+	int fixed, position, aux, smaller, smaller_position;
+	for(fixed = 0; fixed < size - 1; fixed++) {
+		smaller = vet[fixed];
+		smaller_position = fixed;
+		for(position = fixed; position < size; position++) {
+			if (vet[position] < smaller) {
+				smaller = vet[position];
+				smaller_position = position;
 			}
 		}
-		if (smaller_position != position) {
-			aux = vet[position];
-			vet[position] = vet[smaller_position];
+		if (smaller_position != fixed) {
+			aux = vet[fixed];
+			vet[fixed] = vet[smaller_position];
 			vet[smaller_position] = aux;
 		}
 		print_vector(vet, SIZE);
@@ -32,7 +32,7 @@ void selection_sort(int vet[], int size) {
 
 
 int main() {
-    int array[] = {23, 4, 67, -8, 90, 54, 21, 87, 2, -7};
+    int array[] = {23, 4, 67, -8, -5, 54, 21, 87, 2, -7};
     int positions = SIZE, continueTo, cache, num;
     print_vector(array, SIZE);
     selection_sort(array, SIZE);
