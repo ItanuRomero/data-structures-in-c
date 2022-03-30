fb88d4402369cd7323799c167cd577d3049c8978
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2022-03-30 20:37:02 -0300

    add pointer_usage file

---
+++ remembering/pointer_usage.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+	int varA = 40;
+	int varB = 0;
+	int *ptrA = NULL;
+	int *ptrB = NULL;
+	
+	printf("Endereço varA: %d\n", &varA);
+	printf("Endereço varB: %d\n", &varB);
+	printf("Endereço ptrA: %d\n", &ptrA);
+	printf("Endereço ptrB: %d\n", &ptrB);
+	
+	printf("Valor varA: %d\n", varA);
+	printf("Valor varB: %d\n", varB);
+	printf("Valor ptrA: %d\n", ptrA);
+	printf("Valor ptrB: %d\n", ptrB);
+	
+	ptrA = &varA;
+	ptrB = &varB;
+	*ptrA = 30;
+	*ptrB = 45;
+	
+	printf("Endereço varA: %d\n", &varA);
+	printf("Endereço varB: %d\n", &varB);
+	printf("Endereço ptrA: %d\n", &ptrA);
+	printf("Endereço ptrB: %d\n", &ptrB);
+	
+	printf("Valor varA: %d\n", varA);
+	printf("Valor varB: %d\n", varB);
+	printf("Valor ptrA: %d\n", ptrA);
+	printf("Valor ptrB: %d\n", ptrB);
+	
+}
