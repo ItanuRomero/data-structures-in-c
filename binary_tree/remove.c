989967e02356258ffdc6c1baed2cf8a020b0f616
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2022-05-11 20:44:39 -0300

    add third option removal

--- binary_tree/remove.c
+++ binary_tree/remove.c
@@ -78,59 +78,56 @@ Node* biggerOnLeft(Node* n) {
     return n;
 }
 
-void removeNode(Node* n, Node* parent, int valueToRemove) {
-	Node* aux =  (Node*) malloc(sizeof(Node));
-	aux = NULL;
+Node* removeNode(Node* n, Node* parent, int valueToRemove) {
 	if (valueToRemove == n->value) {
 		if (n->left == NULL && n->right == NULL) {
 			if (n->value < parent->value) {
 				parent->left = NULL;
-				free(n);
 			}
 			if (n->value > parent->value) {
 				parent->right = NULL;
-				free(n);
 			}
 		} else if (n->left != NULL && n->right == NULL) {
 			if (n->value < parent->value) {
 				parent->left = n->left;
-				free(n);
 			}
 			if (n->value > parent->value) {
 				parent->right = n->left;
-				free(n);
 			} 
 		} else if (n->left == NULL && n->right != NULL) {
 			if (n->value < parent->value) {
 				parent->left = n->right;
-				free(n);
 			}
 			if (n->value > parent->value) {
 				parent->right = n->right;
-				free(n);
 			}
 		} else if (n->left != NULL && n->right != NULL) {
-			// ainda em construção
+			Node* aux =  (Node*) malloc(sizeof(Node));
+			aux = NULL;
 			aux = biggerOnLeft(n->left);
 			removeNode(n, NULL, aux->value);
 			aux->left = n->left;
 			aux->right = n->right;
-			
 			if (n->value < parent->value) {
 				parent->left = aux;
-				free(n);
 			}
 			if (n->value > parent->value) {
 				parent->right = aux;
-				free(n);
 			}
+			aux = NULL;
 		}
 	} else if (valueToRemove < n->value) {
+		if (n->left == NULL) {
+			return n;
+		}
 		removeNode(n->left, n, valueToRemove);
 	} else if (valueToRemove > n->value) {
+		if (n->right == NULL) {
+			return n;
+		}
 		removeNode(n->right, n, valueToRemove);
 	}
-	free(aux);
+	n = NULL;
 	return n;
 }
 
@@ -140,7 +137,7 @@ void main() {
 
     printf("\nantes de inserir: ");
     preOrder(n);
-    n = insertNode(NULL, 20);
+    n = insertNode(n, 20);
     n = insertNode(n, 10);
     n = insertNode(n, 30);
     n = insertNode(n, 25);
@@ -149,10 +146,10 @@ void main() {
     printf("\ndepois de inserir: ");
     preOrder(n);
     
-    removeNode(n, NULL, 30);
-    printf("\ndepois de remover o 30: ");
+	removeNode(n, NULL, 30);
+    
+	printf("\ndepois de remover o 30: ");
 	preOrder(n);
 	
 	freeAll(n);
 }
-
